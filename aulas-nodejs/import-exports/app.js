475909705fb4f1a33207dd1409e95df1b13b8850
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-03-03 15:23:17 -0300

    modulos

---
+++ aulas-nodejs/import-exports/app.js
@@ -0,0 +1,10 @@
+const {dividir, multiplicar} = require("./functions");
+console.log(dividir(1,2));
+console.log(multiplicar(2,2));
+
+
+// const op = require("./functions");
+// console.log(op.dividir(1,2));
+// console.log(op.multiplicar(2,2));
+
+
---
+++ aulas-nodejs/import-exports/functions.js
@@ -0,0 +1,20 @@
+const somar = function(n1,n2){
+    return n1 + n2;
+}
+
+
+const subtrair = function(n1,n2){
+    return n1 - n2;
+}
+
+const multiplicar = function(n1,n2){
+    return n1 * n2;
+}
+
+const dividir = function(n1,n2){
+    return n1 / n2;
+}
+
+
+module.exports = {dividir, multiplicar};
+
